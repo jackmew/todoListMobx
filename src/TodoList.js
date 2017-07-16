ea9ddcdc1c3ff83a3895d27f7184068b92cf150d
Jackmew <wonderfuljack123@gmail.com>
2017-07-16 14:57:13 +0800

    basic filter

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,14 +3,21 @@ import { observer } from 'mobx-react';
 
 @observer
 export default class TodoList extends React.Component {
+    filter(e) {
+        this.props.store.filter = e.target.value;
+        // console.log(this, e);
+    }    
     render() {
-        const { todos } = this.props.store;
-        const todoList = todos.map((todo) => {
-            return <li>{todo}</li>
+        const { filter, filteredTodos, todos } = this.props.store;
+        const todoList = filteredTodos.map((todo, index) => {
+            return <li key={index}>{todo}</li>
         });
+
         return (
             <div>
                 <h1>Todos</h1>
+                {/*<input className="filter" value={filter} onChange={(e) => this.filter(e)} />*/}
+                <input className="filter" value={filter} onChange={this.filter.bind(this)} />
                 <ul>{todoList}</ul>
             </div>
         );
--- src/TodoStore.js
+++ src/TodoStore.js
@@ -1,8 +1,18 @@
-import { autorun, observable } from 'mobx';
+import { computed, observable } from 'mobx';
 
 class TodoStore {
-    @observable todos = ["buy milk", "buy eggs"];
+    // observable 參數變化，會去重新render畫面
+    @observable todos = ["buy milk", "buy cheese"];
     @observable filter = "";
+    // computed 是因為 @observable filter參數改變，才讓TodoList rerender，這時才再次去計算filterTodos
+    @computed get filteredTodos() {
+        // i: ignore case
+        // 將輸入的this.filter做為條件，只要有一個部分是mapping到一樣的，就會是true
+        var matchesFilter = new RegExp(this.filter, 'i'); 
+        // !this.filter ||  表示 當filter true時 才會去執行後面那行．
+        // todos.filter 會過濾 條件是true的取回來．
+        return this.todos.filter(todo => !this.filter || matchesFilter.test(todo));
+    }
 }
 var store = window.store = new TodoStore;
 
