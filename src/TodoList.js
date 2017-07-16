39adfa87f36f45c06978a5cffdea509cbee0269b
Jackmew <wonderfuljack123@gmail.com>
2017-07-16 15:26:56 +0800

    todo list amazing easy

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,22 +3,33 @@ import { observer } from 'mobx-react';
 
 @observer
 export default class TodoList extends React.Component {
+    createNew(e) {
+        if (e.which == 13) {
+            this.props.store.createTodo(e.target.value);
+            e.target.value = '';
+        }
+    }
     filter(e) {
         this.props.store.filter = e.target.value;
-        // console.log(this, e);
-    }    
+    }
+    toggleComplete(todo) {
+        todo.complete = !todo.complete;
+    }
     render() {
-        const { filter, filteredTodos, todos } = this.props.store;
-        const todoList = filteredTodos.map((todo, index) => {
-            return <li key={index}>{todo}</li>
-        });
+        const { filter, filteredTodos, todos, clearComplete } = this.props.store;
+        const todoList = filteredTodos.map((todo) => (
+            <li key={todo.id}>
+                <input type='checkbox' onChange={this.toggleComplete.bind(this, todo)} value={todo.complete} checked={todo.complete}/> {todo.value}
+            </li>
+        ));
 
         return (
             <div>
                 <h1>Todos</h1>
-                {/*<input className="filter" value={filter} onChange={(e) => this.filter(e)} />*/}
-                <input className="filter" value={filter} onChange={this.filter.bind(this)} />
+                <input className='create' onKeyPress={this.createNew.bind(this)} />
+                <input className='filter' value={filter} onChange={this.filter.bind(this)} />
                 <ul>{todoList}</ul>
+                <a href="#" onClick={clearComplete}>Clear Complete</a>
             </div>
         );
     }
--- src/TodoStore.js
+++ src/TodoStore.js
@@ -1,8 +1,19 @@
 import { computed, observable } from 'mobx';
 
+class Todo {
+    @observable value;
+    @observable id;
+    @observable complete;
+
+    constructor(value) {
+        this.value = value;
+        this.id = Date.now();
+        this.complete = false;
+    }
+}
 class TodoStore {
     // observable 參數變化，會去重新render畫面
-    @observable todos = ["buy milk", "buy cheese"];
+    @observable todos = [];
     @observable filter = "";
     // computed 是因為 @observable filter參數改變，才讓TodoList rerender，這時才再次去計算filterTodos
     @computed get filteredTodos() {
@@ -11,14 +22,17 @@ class TodoStore {
         var matchesFilter = new RegExp(this.filter, 'i'); 
         // !this.filter ||  表示 當filter true時 才會去執行後面那行．
         // todos.filter 會過濾 條件是true的取回來．
-        return this.todos.filter(todo => !this.filter || matchesFilter.test(todo));
+        return this.todos.filter(todo => !this.filter || matchesFilter.test(todo.value));
+    }
+    createTodo(value) {
+        this.todos.push(new Todo(value));
+    }
+    // array.replace() 保持原本的array，只更新內容 被新的array內容取代
+    clearComplete = () => {
+        const inCompleteTodos = this.todos.filter(todo => !todo.complete);
+        this.todos.replace(inCompleteTodos);
     }
 }
 var store = window.store = new TodoStore;
 
 export default store;
-
-// autorun(() => {
-//     console.log(store.filter);
-//     console.log(store.todos[0]);
-// });
